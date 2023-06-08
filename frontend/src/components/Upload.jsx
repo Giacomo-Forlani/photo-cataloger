7f32686e343d588a8ba779a815219d1590cb23c1
Giacomo-Forlani <giacomoforlani11.@gmailcom>
2023-06-08 13:30:15 +0200

    gitignore update

--- frontend/src/components/Upload.jsx
+++ frontend/src/components/Upload.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+const serverUrl = 'http://127.0.0.1:5000'; // l'URL del server Flask
+
 const ImageUpload = () => {
     const [file, setFile] = useState(null);
     const [uploadedFile, setUploadedFile] = useState(null);
@@ -15,15 +17,16 @@ const ImageUpload = () => {
         const formData = new FormData();
         formData.append('image', file);
 
-        const response = await axios.post('http://127.0.0.1:5000/upload', formData);
-        setUploadedFile(response.data.url);
+        const response = await axios.post(`${serverUrl}/upload`, formData);
+        // aggiunge l'URL del server Flask alla URL ritornata
+        setUploadedFile(serverUrl + response.data.url);
     }
 
     return (
         <form onSubmit={handleSubmit}>
             <input type="file" onChange={handleFileChange} />
             <button type="submit">Upload</button>
-            {uploadedFile && <img src={uploadedFile} alt="Uploaded" />}
+            {uploadedFile && <img src={uploadedFile} alt="Uploaded" style={{height: "400px"}}/>}
         </form>
     );
 }
