c77fbb4fd4d8ac62239d634497a7986be2c359b0
Giacomo-Forlani <giacomoforlani11.@gmailcom>
2023-06-19 12:01:05 +0200

    miglioramenti design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Upload from './components/Upload';
 import PoseNet from './components/PoseNet';
+import logo from './logo.svg';
+import imgPlaceholder from './img/imgPlaceholder.svg'
 
 function App() {
     const [uploadedFile, setUploadedFile] = useState(null);
@@ -11,15 +13,22 @@ function App() {
 
     return (
         <div className='grid-container'>
-            <div className='logo'>
-                <div className='img'></div>
+            <div className='logo-container'>
+                <img src={logo} alt="logo" />
                 <h1>Photo-Cataloger</h1>
             </div>
             <div className='searchbar'></div>
             <div className='account'></div>
             <div className='uploader'>
-                {uploadedFile && <PoseNet imageUrl={uploadedFile} />}
-                <Upload onUpload={handleUpload} />
+                <div className='img-container'>
+                    <div className='img-placeholder'>
+                        <img className='imgPlaceholder' src={imgPlaceholder} alt="" />
+                        {uploadedFile && <PoseNet imageUrl={uploadedFile} />}
+                    </div>
+                </div>
+                <div className='uploader-button'>
+                    <Upload onUpload={handleUpload} />
+                </div>
             </div>
             <div className='folders-container'></div>
             <div className='footer'></div>
